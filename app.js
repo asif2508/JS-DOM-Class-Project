906c44494cb8a18633271ee252a3bd6f78568792
asif2508 <rakibul4210@gmail.com>
2025-02-24 12:17:38 +0600

    home page

---
+++ app.js
@@ -0,0 +1,27 @@
+const getProducts = async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const result = await response.json();
+
+  const products = document.getElementById("products");
+  console.log(result);
+
+  result.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.innerHTML = `
+        <div class="border-[1px] border-gray-400 p-4 rounded-[12px] h-[330px] hover:scale-105 duration-300 cursor-pointer">
+        <div>
+        <img src="${product.image}" class="h-[200px] w-[300px]" alt="" />
+        </div>
+        <div class="p-3">
+    <h3 class="font-semibold">${product.title.length > 40 ? product.title.slice(0, 40) + "..." : product.title}</h3>
+
+    <h1 class="font-bold text-[20px]">$${product?.price}</h1>
+        </div>
+        </div>
+        `;
+
+    products.appendChild(productCard);
+  });
+};
+
+getProducts();
